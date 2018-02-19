3b5333ed42ed0c7e7e0e0537f0b4298c52c5fb5b
cbourget <charleseric.bourget@kovaro.ca>
2018-02-18 21:34:59 -0500

    annotyped decorator

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+
+package_name = 'annotype'
+
+install_requires = [
+    'marshmallow>=3.0.0b7',
+    'wheel'
+]
+
+extras_require={
+    'tests': [
+        'pytest',
+        'coverage',
+        'pytest-cov'
+    ]
+}
+
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    author='Charles-Éric Bourget',
+    author_email='charlesericbourget@gmail.com',
+    description='Marshmallow and Python 3 annotations',
+    license='TBD',
+    classifiers=[
+        'Private :: Do Not Upload to pypi server'
+    ],
+    install_requires=install_requires,
+    extras_require=extras_require
+)
---
+++ tests/test_annotype.py
@@ -0,0 +1,63 @@
+import pytest
+
+from annotype import annotyped
+from marshmallow import (
+    Schema,
+    ValidationError,
+    fields
+)
+
+
+class SchemaForB(Schema):
+    ba = fields.Str()
+    bb = fields.Int(required=True)
+
+
+@annotyped()
+def foo(a: fields.Str(), b: SchemaForB, c: fields.Str() = None):
+    return (a, b, c)
+
+
+@annotyped()
+def bar(a):
+    return a
+
+
+def test_invalid_arguments():
+    # a should be a string but is an integer
+    with pytest.raises(ValidationError) as exc_info:
+        foo(1, {'ba': 'world', 'bb': 1})
+
+    # b should be a dict with the key 'bb'
+    with pytest.raises(ValidationError) as exc_info:
+        foo('hello', {'ba': 'world'})
+
+    # c is a keyword argument and should be a string
+    with pytest.raises(ValidationError) as exc_info:
+        foo('hello', {'ba': 'world', 'bb': 1}, c=1)
+
+
+def test_valid_arguments():
+    # all arguments are valid
+    assert foo('hello', {'ba': 'world', 'bb': 1}) == (
+        'hello',
+        {'ba': 'world', 'bb': 1},
+        None
+    )
+
+    # b.bb is a string but marshmallow formats automatically it to an integer
+    assert foo('hello', {'ba': 'world', 'bb': '1'}) == (
+        'hello',
+        {'ba': 'world', 'bb': 1},
+        None
+    )
+
+    # all arguments are passed as keyword arguments
+    assert foo(b={'ba': 'world', 'bb': '1'}, c='1', a='hello') == (
+        'hello',
+        {'ba': 'world', 'bb': 1},
+        '1'
+    )
+
+    # function as no annotation
+    assert bar('hello') == 'hello'
